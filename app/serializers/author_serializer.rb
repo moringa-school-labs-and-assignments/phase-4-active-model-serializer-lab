18edca5d44276d3946da50d974d2b2f6264e9d34
joshhoneypot <joshuaangugo@gmail.com>
2023-06-28 15:34:20 +0300

    Assignment done

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,16 @@
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :name, :profile, :posts
+
+  has_one :profile
+  has_many :posts
+
+  def posts
+    object.posts.map do |post|
+      {
+        title: post.title,
+        short_content: "#{post.content[0..39]}...",
+        tags: post.tags.map { |tag| { name: tag.name } }
+      }
+    end
+  end
+end
